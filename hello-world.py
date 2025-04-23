3d4e106aab766f73b692da912c58ac32d0d583e3
AntonyRiwi <antony.martinez@riwi.io>
2025-04-22 20:49:13 -0500

    Add files via upload

---
+++ hello-world.py
@@ -0,0 +1,13 @@
+print('Hello World')
+
+name = input("Escribe tu nombre: ")
+
+print(type(name).__name__)
+
+
+if name == "Antony":
+    print("Eres Antony")
+elif name == "Pedro":
+    print("Eres Pedro")
+else:
+    print("No eres ANtony")
